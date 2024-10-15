618f0d31aac37b7a3552563d7d29fe0af5b48230
Lucas Thébault <lucas.thebault22@gmail.com>
2024-10-15 16:31:31 +0200

    Bouton ui

--- components/atoms/ThemedButton.tsx
+++ components/atoms/ThemedButton.tsx
@@ -1,12 +1,22 @@
-import {Button, GestureResponderEvent, Pressable} from "react-native";
+import {Button, GestureResponderEvent, Pressable, StyleProp, ViewStyle} from "react-native";
 import {ReactNode} from "react";
+import {useTheme} from "@/hooks/useThemeColor";
 
 interface Props{
     onPress:(event:GestureResponderEvent)=>void,
-    children:ReactNode
+    children:ReactNode,
+    style?:StyleProp<ViewStyle>
 }
-export const ThemedButton = ({children,onPress}:Props)=>{
-    return <Pressable onPress={onPress}>
+export const ThemedButton = ({children,onPress, style}:Props)=>{
+    const {colors}= useTheme()
+    return <Pressable onPress={onPress} style={[{
+        backgroundColor:colors.border,
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:10,
+        paddingHorizontal:10,
+        paddingVertical:5,
+    }, style]}>
         {children}
     </Pressable>
 }

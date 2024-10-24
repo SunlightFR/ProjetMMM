c75c1ed19fdd4cfff37b42cbaa8fd118989b6d52
Lucas Thébault <lucas.thebault22@gmail.com>
2024-10-24 16:21:44 +0200

    Styles

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import {Project} from "@/api/models/Project";
-import {ActivityIndicator, Text, View, StyleSheet, TouchableOpacity, GestureResponderEvent} from "react-native";
+import {ActivityIndicator, GestureResponderEvent, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {useUser} from "@/contexts/UserContext";
 import {TextWithIcon} from "@/components/atoms/TextWithIcon";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -7,44 +7,52 @@ import {useTheme} from "@/hooks/useThemeColor";
 import {ProjectStatusIcon} from "@/components/atoms/ProjectStatus";
 import {WarningIcon} from "@/components/atoms/WarningIcon";
 import {useProjects} from "@/contexts/ProjectsContext";
+import {Loader} from "@/components/atoms/Loader";
+import {ThemedText} from "@/components/ThemedText";
 
 interface Props {
-    project:Project,
-    onPress:(event:GestureResponderEvent)=>void
+    project: Project,
+    onPress: (event: GestureResponderEvent) => void
 }
-export const ProjectCard = ({project, onPress}:Props)=>{
+
+export const ProjectCard = ({project, onPress}: Props) => {
     const theme = useTheme();
     const user = useUser()
     const projects = useProjects()
 
-    if(!user.current) return <View><ActivityIndicator></ActivityIndicator></View>
+    if (!user.current) return <View><Loader/></View>
+
     return <TouchableOpacity style={[
         {
-            borderColor:theme.colors.border,
-            shadowColor:theme.colors.shadow,
-            backgroundColor:theme.colors.background
+            borderColor: theme.colors.border,
+            shadowColor: theme.colors.shadow,
+            backgroundColor: theme.colors.background
         },
         styles.container
     ]} onPress={onPress}>
-        <Text style={[
-            {color:theme.colors.text},
+        <ThemedText style={[
+            {color: theme.colors.text},
             styles.objectText
-        ]}>{project.object}</Text>
+        ]}>{project.object}</ThemedText>
         <TextWithIcon
             icon={<Ionicons name={"calendar-outline"} color={theme.colors.text} size={20}></Ionicons>}
             text={project.location}
+            viewStyle={{marginTop: 8}}
         ></TextWithIcon>
         <TextWithIcon
             icon={<Ionicons name={"location-outline"} color={theme.colors.text} size={20}></Ionicons>}
             text={project.location}
+            viewStyle={{marginTop: 8}}
         ></TextWithIcon>
         <TextWithIcon
             icon={<Ionicons name={"person-circle-outline"} color={theme.colors.text} size={20}></Ionicons>}
-            text={projects.getUserById(user.current.role==="supervisor" ? project.manager_id:project.supervisor_id)?.firstName}
+            text={projects.getUserById(user.current.role === "supervisor" ? project.manager_id : project.supervisor_id)?.firstName}
+            viewStyle={{marginTop: 8}}
         ></TextWithIcon>
         <TextWithIcon
             icon={<Ionicons name={"call-outline"} color={theme.colors.text} size={20}></Ionicons>}
             text={project.clientNumber}
+            viewStyle={{marginVertical: 8}}
         ></TextWithIcon>
         <View style={styles.bottom}>
             <ProjectStatusIcon status={project.status}></ProjectStatusIcon>
@@ -54,27 +62,27 @@ export const ProjectCard = ({project, onPress}:Props)=>{
 }
 
 const styles = StyleSheet.create({
-    container:{
-        borderRadius:10,
-        borderWidth:2,
-        marginHorizontal:"auto",
-        marginBottom:15,
-        padding:10,
+    container: {
+        borderRadius: 10,
+        borderWidth: 2,
+        marginHorizontal: 10,
+        marginBottom: 15,
+        padding: 10,
 
-        elevation:5,
-        shadowRadius:5,
-        shadowOffset:{
-            width:1,
-            height:1
+        elevation: 5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 1,
+            height: 1
         },
-        shadowOpacity:0.8
+        shadowOpacity: 0.8
     },
-    objectText:{
-        fontSize:20
+    objectText: {
+        fontSize: 20
     },
-    bottom:{
-        marginTop:4,
-        flexDirection:'row',
-        justifyContent:"space-between"
+    bottom: {
+        marginTop: 4,
+        flexDirection: 'row',
+        justifyContent: "space-between"
     }
 })
